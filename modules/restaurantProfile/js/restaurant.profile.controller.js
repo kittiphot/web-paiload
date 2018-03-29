26fa7a97962a90aca814ee67dce801ad1bf9acb3
Maha <acs57011210118@gmail.com>
2018-03-29 17:28:43 +0700

    .

--- modules/restaurantProfile/js/restaurant.profile.controller.js
+++ modules/restaurantProfile/js/restaurant.profile.controller.js
@@ -1,15 +1,15 @@
-angular.module('myApp').controller('RestaurantsProfileController', ['$scope', 'RestaurantsProfileFactory', '$routeParams', '$window', function ($scope, hotelProfileFactory, $routeParams, $window) {
+angular.module('myApp').controller('RestaurantsProfileController', ['$scope', 'RestaurantsProfileFactory', '$routeParams', '$window', function ($scope, RestaurantsProfileFactory, $routeParams, $window) {
   $scope.RestaurantProfiles = {};
   $scope.status = (typeof $routeParams.status != 'undefined' ? $routeParams.status : 0);
   $scope.id = (typeof $routeParams.id != 'undefined' ? $routeParams.id : 0);
 
   $scope.getRestaurantsProfile = function () {
-    hotelProfileFactory.getRestaurantsProfile($scope.id).then(function successCallback(res) {
+    RestaurantsProfileFactory.getRestaurantsProfile($scope.id).then(function successCallback(res) {
       console.log(res.data)
       $scope.RestaurantProfiles.name = res.data.name;
       $scope.RestaurantProfiles.mobile = res.data.mobile;
       $scope.RestaurantProfiles.phone = res.data.phone;
-      $scope.RestaurantProfiles.old_image = res.data.image;
+      $scope.RestaurantProfiles.image = res.data.image;
       $scope.RestaurantProfiles.address = res.data.address;
       $scope.RestaurantProfiles.latitude = res.data.latitude;
       $scope.RestaurantProfiles.longitude = res.data.longitude;
@@ -28,7 +28,7 @@ angular.module('myApp').controller('RestaurantsProfileController', ['$scope', 'R
       id: $scope.id,
       name: (typeof $scope.RestaurantProfiles.name != 'undefined' ? $scope.RestaurantProfiles.name : ''),
       image: (typeof $scope.RestaurantProfiles.image != 'undefined' ? $scope.RestaurantProfiles.image : $scope.hotelProfiles.old_image),
-      old_image: ($scope.id == 0 ? 'default_small.png' : $scope.RestaurantProfiles.old_image),
+      // old_image: ($scope.id == 0 ? 'default_small.png' : $scope.RestaurantProfiles.old_image),
       mobile: (typeof $scope.RestaurantProfiles.mobile != 'undefined' ? $scope.RestaurantProfiles.mobile : ''),
       phone: (typeof $scope.RestaurantProfiles.phone != 'undefined' ? $scope.RestaurantProfiles.phone : ''),
       address: (typeof $scope.RestaurantProfiles.address != 'undefined' ? $scope.RestaurantProfiles.address : ''),
@@ -40,7 +40,7 @@ angular.module('myApp').controller('RestaurantsProfileController', ['$scope', 'R
       detail: (typeof $scope.RestaurantProfiles.detail != 'undefined' ? $scope.RestaurantProfiles.detail : ''),
     }
     console.log(params);
-    hotelProfileFactory.setHotel(params).then(function successCallback(res) {
+    RestaurantsProfileFactory.setRestaurants(params).then(function successCallback(res) {
       console.log(res.data)
       if ($scope.status != 0 && $scope.id != 0) {
         $window.location.href = '#!/about/' + $scope.status + '/' + $scope.id;
--- modules/restaurantProfile/js/restaurant.profile.factory.js
+++ modules/restaurantProfile/js/restaurant.profile.factory.js
@@ -1,13 +1,13 @@
 angular.module('myApp').factory('RestaurantsProfileFactory', ['$http', 'Upload', function ($http, Upload) {
   // var api = 'http://103.13.228.76/web/kk_api/public/index.php';
   var api = 'http://localhost:9000';
-  var hotelProfileFactory = {};
+  var RestaurantsProfileFactory = {};
 
-  hotelProfileFactory.getRestaurantsProfile = function (id) {
+  RestaurantsProfileFactory.getRestaurantsProfile = function (id) {
     return $http.get(api + '/restaurants/' + id);
   }
 
-  hotelProfileFactory.setRestaurants = function (params) {
+  RestaurantsProfileFactory.setRestaurants = function (params) {
     let status = (params.id == 0 ? 'create' : 'edit');
     return Upload.upload({
       url: api + '/restaurants/' + status,
@@ -15,13 +15,5 @@ angular.module('myApp').factory('RestaurantsProfileFactory', ['$http', 'Upload',
     });
   }
 
-  // hotelProfileFactory.getHotelAlbums = function (id) {
-  //   return $http.get(api + '/hotel_profile_albums/' + id);
-  // }
-
-  // hotelProfileFactory.getHotelImages = function (id) {
-  //   return $http.get(api + '/hotel_albums/' + id);
-  // }
-
-  return hotelProfileFactory;
+  return RestaurantsProfileFactory;
 }]);
